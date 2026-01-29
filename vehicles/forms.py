587400b8044d9c0d4118af1aca35ae7887586d78
Meiryanne Martins <meirympereira@gmail.com>
2026-01-29 17:48:23 -0300

    refactore: form to register vehicle

--- vehicles/forms.py
+++ vehicles/forms.py
@@ -2,24 +2,6 @@ from django import forms
 from vehicles.models import Brand, Vehicle
 
 class VehicleForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    price = forms.FloatField()
-    photo = forms.ImageField()
-
-    def save(self):
-        vehicle = Vehicle(
-            model = self.cleaned_data['model'],
-            brand = self.cleaned_data['brand'],
-            factory_year = self.cleaned_data['factory_year'],
-            model_year = self.cleaned_data['model_year'],
-            plate = self.cleaned_data['plate'],
-            price = self.cleaned_data['price'],
-            photo = self.cleaned_data['photo']
-        )
-
-        vehicle.save()
-        return vehicle
+    class Meta:
+        model = Vehicle
+        fields = '__all__'
