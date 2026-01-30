5265c449b7db14b1bd82ffd70865d9ca9ba49dd0
Meiryanne Martins <meirympereira@gmail.com>
2026-01-30 13:26:04 -0300

    feat: add form to register user

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts.views import register_view
 
 from vehicles.views import new_vehicle_view
 from vehicles.views import vehicles_view
@@ -10,4 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('vehicles/', vehicles_view, name='vehicles_list'),
     path('new_vehicle/', new_vehicle_view, name='new_vehicle'),
+    path('register/', register_view, name='register')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
