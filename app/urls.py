f9d58a639320bb8be9ae2de4fc3adf023d7a3d16
Meiryanne Martins <meirympereira@gmail.com>
2026-01-30 19:49:04 -0300

    feat: add rote to login and logout

--- app/urls.py
+++ app/urls.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from accounts.views import register_view
+from accounts.views import register_view, login_view, logout_view
+from vehicles.views import new_vehicle_view, vehicles_view
 
-from vehicles.views import new_vehicle_view
-from vehicles.views import vehicles_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vehicles/', vehicles_view, name='vehicles_list'),
     path('new_vehicle/', new_vehicle_view, name='new_vehicle'),
-    path('register/', register_view, name='register')
+    path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
+    path('logout/', logout_view, name='logout')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
