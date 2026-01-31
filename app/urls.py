89047a07a2b6180abbf3e52fbd06d5fef7c6de94
Meiryanne Martins <meirympereira@gmail.com>
2026-01-31 10:57:02 -0300

    refactore: function view get vehicles to class view

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import register_view, login_view, logout_view
-from vehicles.views import new_vehicle_view, vehicles_view
+from vehicles.views import new_vehicle_view, VehiclesView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vehicles/', vehicles_view, name='vehicles_list'),
+    path('vehicles/', VehiclesView.as_view(), name='vehicles_list'),
     path('new_vehicle/', new_vehicle_view, name='new_vehicle'),
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
--- vehicles/views.py
+++ vehicles/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render, redirect
 from vehicles.models import Vehicle
 from vehicles.forms import VehicleForm
 
-def vehicles_view(request):
-    vehicles = Vehicle.objects.all().order_by('model')
-    search = request.GET.get('search')
+from django.views import View
 
-    if search:
-        vehicles = vehicles.filter(model__icontains=search)
 
-    return render(
-        request,
-        'vehicles.html',
-        {'vehicles': vehicles}
-    )
+class VehiclesView(View):
+    def get(self, request):
+        vehicles = Vehicle.objects.all().order_by('model')
+        search = request.GET.get('search')
+
+        if search:
+            vehicles = vehicles.filter(model__icontains=search)
+
+        return render(
+            request,
+            'vehicles.html',
+            {'vehicles': vehicles}
+        )    
+
 
 def new_vehicle_view(request):
     if request.method == 'POST':
