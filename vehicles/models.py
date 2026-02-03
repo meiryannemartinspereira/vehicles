9762444d896b9f171effa655f475395ee57ee8ec
Meiryanne Martins <meirympereira@gmail.com>
2026-02-03 14:13:35 -0300

    feat: add bio to vehicle

--- vehicles/models.py
+++ vehicles/models.py
@@ -16,6 +16,7 @@ class Vehicle(models.Model):
     plate = models.CharField(max_length=20,blank=True, null=True)
     price = models.FloatField(blank=True, null=True)
     photo = models.ImageField(upload_to='vehicles/', blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.model
